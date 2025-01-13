25d4a16716d6d54bd46dd40442f06b99fbf713aa
Zeke Palmer <94257500+EPalmer14@users.noreply.github.com>
2025-01-12 19:55:01 -0500

    Time: 11 ms (92.16%), Space: 14.1 MB (69.6%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = n  # At least one candy per kid
+        i = 1
+
+        while i < n:
+            if (ratings[i] == ratings[i - 1]):
+                i += 1
+                continue
+
+            increasing_neighbors = 0
+            while (i < n and ratings[i] > ratings[i - 1]):
+                increasing_neighbors += 1
+                candies += increasing_neighbors
+                i += 1
+            
+            if (i == n):
+                return  candies
+            
+            decreasing_neighbors = 0
+            while (i < n and ratings[i] < ratings[i - 1]):
+                decreasing_neighbors += 1
+                candies += decreasing_neighbors
+                i += 1
+
+            candies -= min(increasing_neighbors, decreasing_neighbors)
+
+        return candies
+                
+                
+
+        
+        if(ratings[0] <= ratings[1]):
+            candies += 1
+        else:
+            candies += 2
+
+        if(ratings[-1] <= ratings[-2]):
+            candies += 1
+        else:
+            candies += 2
+        
+        return candies
+        
