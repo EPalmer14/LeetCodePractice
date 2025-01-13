6320f8b64e90140b1a5515f024ae8f7625fc6621
Zeke Palmer <94257500+EPalmer14@users.noreply.github.com>
2025-01-12 21:32:02 -0500

    Time: 7 ms (50.58%), Space: 12.8 MB (54.29%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -6,11 +6,14 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(numbers)
+        out = [0] * 2
         L, R = 0, n - 1
 
         while (L < R):
             if (numbers[L] + numbers[R] == target):
-                return [L + 1, R + 1]
+                out[0] = L + 1
+                out[1] = R + 1
+                return out
             elif (numbers[L] + numbers[R] > target):
                 R -= 1
             else:
